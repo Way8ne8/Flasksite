31b0555da53e798bcc1d608991bbeb61f8f3d1e2
SIA <way8ne8@gmail.com>
2021-07-22 17:24:38 +0300

    Flask site 0.0.2

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,13 +8,23 @@ fmenu = ["О компании", "Контакты", "ББ"]
 
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template('index.html', title="Про фласку", menu=menu, fmenu=fmenu)
-
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', about="Свистит...", amenu=amenu, fmenu=fmenu)
 
+@app.route("/profile/<username>")
+def profile(username):
+    # return f"Пользователь: {username}"
+    return render_template('user.html', username=username)
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('profile', username="selfedu"))
 
 if __name__ == "__main__":
     app.run(debug=True)
