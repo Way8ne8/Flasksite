60d13471ef78ec3edde13a25697b6a2ae0da5422
SIA <way8ne8@gmail.com>
2021-07-26 17:19:14 +0300

    Flask site 0.0.7

---
+++ admin/admin.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, render_template, url_for, redirect, session, request, flash, g
+
+admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+
+menu = [{'url': '.index', 'title': 'Панель'},
+        {'url': '.logout', 'title': 'Выйти'}]
+
+@admin.route('/')
+def index():
+    if not isLogged():
+        return redirect(url_for('.login'))
+
+    return render_template('admin/index.html', menu=menu, title='Админ-панель')
+
+@admin.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        if request.form['user'] == "admin" and request.form['psw'] == "12345":
+            login_admin()
+            return redirect(url_for('.index'))
+        else:
+            flash("Неверная пара логин/пароль", "error")
+
+    return render_template('admin/login.html', title='Админ-панель')
+
+def login_admin():
+    session['admin_logged'] = 1
+
+def isLogged():
+    return True if session.get('admin_logged') else False
+
+def logout_admin():
+    session.pop('admin_logged', None)
+
+
+@admin.route('/logout', methods=["POST", "GET"])
+def logout():
+    if not isLogged():
+        return redirect(url_for('.login'))
+    logout_admin()
+    return redirect(url_for('.login'))
--- flsite.py
+++ flsite.py
@@ -1,26 +1,41 @@
-import sqlite3, os
-from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g, sessions
-from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+import os
+from flask import Flask, render_template, request, g, flash, abort, redirect, url_for, make_response, session
 from FDataBase import FDataBase
-from flask_login import LoginManager, login_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from UserLogin import UserLogin
+from forms import LoginForm, RegisterForm
+from admin.admin import admin
+from werkzeug.datastructures import MultiDict
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
 SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
-USERNAME = 'admin'
-PASSWORD = '123'
+MAX_CONTENT_LENGTH = 1024 * 1024
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
+
+app.register_blueprint(admin, url_prefix='/admin')
+
 login_manager = LoginManager(app)
-fmenu = ["О компании", "Контакты", "ББ"]
+login_manager.login_view = 'login'
+login_manager.login_message = "Авторизуйтесь для доступа к закрытым страницам"
+login_manager.login_message_category = "success"
+
 @login_manager.user_loader
 def load_user(user_id):
     print('load_user')
     return UserLogin().fromDB(user_id, dbase)
 
+@login_manager.user_loader
+def load_user(user_id):
+    print("load_user")
+    return UserLogin().fromDB(user_id, dbase)
+
+fmenu = ["О компании", "Контакты", "ББ"]
 
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
@@ -37,6 +52,7 @@ def create_db():
 
 @app.route("/")
 def index():
+    session.clear()
     if 'visits' in session:
         session['visits'] = session.get('visits') + 1  # обновление данных сессии
     else:
@@ -98,39 +114,79 @@ def showPost(alias):
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    if request.method == 'POST':
-        user = dbase.getUserByEmail(request.form['email'])
-        if user and check_password_hash(user['psw'], request.form['psw']):
-            userLogin = UserLogin().create(user)
-            login_user(userLogin)
-            return redirect(url_for('index'))
-        flash('Ошибка авторизации', category='error')
-    return render_template('login.html', title="Авторизация", menu=dbase.getMenu())
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = dbase.getUserByEmail(form.email.data)
+        if user and check_password_hash(user['psw'], form.psw.data):
+            userlogin = UserLogin().create(user)
+            rm = form.remember.data
+            login_user(userlogin, remember=rm)
+            return redirect(request.args.get("next") or url_for("profile"))
+
+        flash("Неверная пара логин/пароль", "error")
+
+    return render_template("login.html", menu=dbase.getMenu(), title="Авторизация", form=form)
+
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
-    if request.method == "POST":
-        if len(request.form['name']) > 4 and len(request.form['email']) > 4 \
-            and len(request.form['psw']) > 4 and request.form['psw'] == request.form['psw2']:
-            hash = generate_password_hash( request.form['psw'])
-            res = dbase.addUser(request.form['name'], request.form['email'], hash)
+    form = RegisterForm()
+    if form.validate_on_submit():
+            hash = generate_password_hash(request.form['psw'])
+            res = dbase.addUser(form.name.data, form.email.data, hash)
             if res:
-                flash("Вы успешно зарегестрированы", "success")
+                flash("Вы успешно зарегистрированы", "success")
                 return redirect(url_for('login'))
             else:
-                flash("Ошибка при регистрации", "error")
-    return render_template('register.html', title="Авторизация", menu=dbase.getMenu())
+                flash("Ошибка при добавлении в БД", "error")
 
-@app.route("/profile/<username>")
-def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(401)
-    print(render_template('user.html', username=username, menu=menu, fmenu=fmenu))
-    return render_template('user.html', username=username, menu=menu, fmenu=fmenu)
+    return render_template("register.html", menu=dbase.getMenu(), title="Регистрация", form=form)
 
-# @app.errorhandler(404)
-# def pageNotFount(error):
-#     return render_template('page404.html', title="Страница не найдена", menu=menu), 404
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Вы вышли из аккаунта", "success")
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template("user.html", menu=dbase.getMenu(), title="Профиль")
+
+
+@app.route('/userava')
+@login_required
+def userava():
+    img = current_user.getAvatar(app)
+    if not img:
+        return ""
+
+    h = make_response(img)
+    h.headers['Content-Type'] = 'image/png'
+    return h
+
+@app.route('/upload', methods=["POST", "GET"])
+@login_required
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and current_user.verifyExt(file.filename):
+            try:
+                img = file.read()
+                res = dbase.updateUserAvatar(img, current_user.get_id())
+                if not res:
+                    flash("Ошибка обновления аватара", "error")
+                flash("Аватар обновлен", "success")
+            except FileNotFoundError as e:
+                flash("Ошибка чтения файла", "error")
+        else:
+            flash("Ошибка обновления аватара", "error")
+
+    return redirect(url_for('profile'))
 
 if __name__ == "__main__":
     app.run(debug=True)
